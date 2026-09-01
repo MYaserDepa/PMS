7645d37d75c2e7fb36efe2e23ebffa5e6173db28
Mohammad Yaser Azrak <yaserazrak5@gmail.com>
2026-09-01 09:59:41 +0400

    Add emp GIDs needed for a full test

---
+++ test.js
@@ -0,0 +1,44 @@
+const tests = [
+    {
+        "EMPLOYEE_NUMBER": "22465", // DUG Leadership
+        "FULL_NAME": "Cleopetra Abu Thalam",
+        "DEPARTMENT": "Group People and Culture - Depa Limited",
+        "SUPERVISOR_NO": "22360",
+        "SUPERVISOR": "Khalil Safwan Khalil Saket",
+    },
+    {
+        "EMPLOYEE_NUMBER": "19656", // Dept Head
+        "FULL_NAME": "Abdul Fathah Kodappanakkal",
+        "DEPARTMENT": "HR - Carrara Mid -East",
+        "SUPERVISOR_NO": "15621",
+        "SUPERVISOR": "Muhieddine Morjan",
+    },
+    {
+        "EMPLOYEE_NUMBER": "4090", // KBU Leadership
+        "FULL_NAME": "Hassnien Ibrahim Mohamed Mahdy",
+        "DEPARTMENT": "MDs Office - Depa Interiors (L.L.C)",
+        "SUPERVISOR_NO": "22360",
+        "SUPERVISOR": "Khalil Safwan Khalil Saket",
+    },
+    {
+        "EMPLOYEE_NUMBER": "22360",
+        "FULL_NAME": "Khalil Safwan Khalil Saket",
+        "DEPARTMENT": "CEO's Office - Depa Limited",
+        "SUPERVISOR_NO": null,
+        "SUPERVISOR": "BOD",
+    },
+    {
+        "EMPLOYEE_NUMBER": "21975", // Admin
+        "FULL_NAME": "Mohammad Yaser Azrak",
+        "DEPARTMENT": "Information Technology - Depa Limited",
+        "SUPERVISOR_NO": "15654",
+        "SUPERVISOR": "Kamran Ali Khan",
+    },
+    {
+        "EMPLOYEE_NUMBER": "14077", // Professional
+        "FULL_NAME": "Qazi Zaeem Qazi Aleem Ullah",
+        "DEPARTMENT": "Information Technology - Depa Limited",
+        "SUPERVISOR_NO": "22360",
+        "SUPERVISOR": "Khalil Safwan Khalil Saket",
+    }
+]
